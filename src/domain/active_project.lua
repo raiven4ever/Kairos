6dc6ebc78524fc53fffc16fd9d952cf7ae135828
craponyourlap <143549907+craponyourlap@users.noreply.github.com>
2026-03-15 23:11:11 -0400

    spacing lol

--- src/domain/active_project.lua
+++ src/domain/active_project.lua
@@ -18,6 +18,7 @@ function module:start(proj: Project)
 		proj and not module.current_project and not module.connection,
 		"Project must be provided, and there must be no current project or connection"
 	)
+
 	local function modify_active_days()
 		local current = DateTime.now()
 		local same_day
@@ -27,6 +28,7 @@ function module:start(proj: Project)
 			else
 				local current_local_time = current:ToLocalTime() :: DateTimeData
 				local last_worked_local_time = proj.LastWorked:ToLocalTime() :: DateTimeData
+
 				same_day = current_local_time.Day == last_worked_local_time.Day
 					and current_local_time.Year == last_worked_local_time.Year
 					and current_local_time.Month == last_worked_local_time.Month
@@ -38,15 +40,20 @@ function module:start(proj: Project)
 	end
 
 	module.current_project = proj
+
 	-- modify proj
 	proj.Sessions += 1
+
 	if not proj.FirstSessionDate then
 		proj.FirstSessionDate = DateTime.now()
 	end
+
 	modify_active_days()
+
 	if not proj.LastWorked then
 		proj.LastWorked = DateTime.now()
 	end
+
 	module.connection = RunService.Heartbeat:Connect(function(delta_time: number)
 		proj.TotalTime += delta_time
 		module.current_session_time += delta_time
@@ -55,11 +62,14 @@ end
 
 function module:terminate()
 	local proj = module.current_project
+
 	assert(proj and module.connection, "No active project or connection to terminate")
 	module.connection:Disconnect()
+
 	-- modify proj
 	proj.AverageSessionLength = proj.TotalTime / proj.Sessions
 	proj.LongestSession = math.max(proj.LongestSession, module.current_session_time)
+
 	module.current_project = nil
 end
 
