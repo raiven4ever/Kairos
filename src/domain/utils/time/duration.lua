0a23eda272c4b7f2e0fd56c7f14c6a458f5fe112
craponyourlap <143549907+craponyourlap@users.noreply.github.com>
2026-03-17 22:04:06 -0400

    -- TODO: update based on the new functions

--- src/domain/utils/time/duration.lua
+++ src/domain/utils/time/duration.lua
@@ -10,7 +10,6 @@ export type Duration = {
 
 local module = {}
 
--- TODO: update based on the new functions
 function module:get(from: DateTime, to: DateTime)
 	local from_seconds = from.UnixTimestamp
 	local to_seconds = to.UnixTimestamp
@@ -18,24 +17,7 @@ function module:get(from: DateTime, to: DateTime)
 
 	local duration_number = to_seconds - from_seconds
 
-	local function mod_then_divide(val: number)
-		local to_return = duration_number % val
-		duration_number /= val
-
-		return to_return
-	end
-
-	local sec = mod_then_divide(60)
-	local min = mod_then_divide(60)
-	local hour = mod_then_divide(24)
-	local day = duration_number -- what's left
-
-	return {
-		Day = day,
-		Hour = hour,
-		Minute = min,
-		Second = sec,
-	} :: Duration
+	return module:from_seconds(duration_number)
 end
 
 function module:from_seconds(amount: number)
@@ -62,7 +44,6 @@ function module:from_seconds(amount: number)
 	} :: Duration
 end
 
--- TODO: function to
 function module:to_seconds(duration: Duration) -- this is so fucking trivial, this shouldn't be a function outside of wanting symmetry
 	return duration.TotalSeconds
 end
