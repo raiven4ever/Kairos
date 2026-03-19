0f9ea4b88dcaa2b70937ad6be39dfd9f3a186504
craponyourlap <143549907+craponyourlap@users.noreply.github.com>
2026-03-18 20:02:49 -0400

    why did i still have those fuckass files?

--- src/__tests__/sum.spec.lua
+++
@@ -1,8 +0,0 @@
-local ServerScriptService = game:GetService("ServerScriptService")
-local jest_globals = require(ServerScriptService.Kairos.packages["jest-globals"])
-local expect = jest_globals.expect
-local it = jest_globals.it
-
-it("adds 1 + 2 to equal 3", function()
-	expect(1 + 2).toBe(3)
-end)
